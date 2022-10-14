15aa88c47b93045189f07271f00c664041d366d9
viraj <jeffhbk420@gmail.com>
2022-10-14 12:23:22 +0530

    Updates

---
+++ precache-manifest.b86f4f20169e4d079e046cb8d31052c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07cb755b1e909dd2ff7c",
+    "url": "/textutils/static/css/main.aa8327af.chunk.css"
+  },
+  {
+    "revision": "07cb755b1e909dd2ff7c",
+    "url": "/textutils/static/js/main.07cb755b.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/textutils/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "4d3b0996b255194c96dd",
+    "url": "/textutils/static/js/2.4d3b0996.chunk.js"
+  },
+  {
+    "revision": "630f10c0371371d002cf",
+    "url": "/textutils/static/js/runtime~main.630f10c0.js"
+  },
+  {
+    "revision": "11fdbb64685dff05f56326b59e2a42f7",
+    "url": "/textutils/index.html"
+  }
+];
